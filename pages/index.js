b77a08399e64de23e585cab4f8989113ef321bb5
hadi-a-azeez <hadikoduvally@gmail.com>
2021-08-18 04:59:45 -0700

    home page on process

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import NavBar from '../components/NavBar'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import NavBar from "../components/NavBar";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -14,8 +14,34 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello world</h1>
+        <h1 className={styles.main_heading}>
+          Embeddable <a className={styles.link_text}>video</a> widgets <br></br>
+          for your website
+        </h1>
+        <div className={styles.video_container}>
+          <div className={styles.arrow_wraper}>
+            <h1 className={styles.arrow_text}>TRY IT OUT</h1>
+            <img
+              src="https://assets.website-files.com/6080285e10b3ca5844aecb46/6081314b013edff5de11aaeb_colorful_arrow.svg"
+              loading="lazy"
+              width="68"
+              alt=""
+              className={styles.arrow_img}
+            ></img>
+          </div>
+          <video width="100%" height="100%" className={styles.video} controls>
+            <source
+              src="https://assets.mixkit.co/videos/preview/mixkit-face-of-a-fighter-exhausted-during-a-fight-40988-large.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <h1 className={styles.sub_heading}>
+          An embeddable video widget for all your needs with <br></br>video
+          hosting to custom styling
+        </h1>
+        <button className={styles.btn_main}>Try it for free</button>
       </main>
     </div>
-  )
+  );
 }
