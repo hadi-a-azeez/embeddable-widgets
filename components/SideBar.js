ef0dcace593fa0ecefb75d60c75d32dc592b13fd
hadi-a-azeez <hadikoduvally@gmail.com>
2021-08-26 08:51:42 -0700

    adding sideBar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,7 +1,14 @@
 import styles from "./sideBar.module.scss";
 
 const SideBar = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item_wraper}>
+        <img className={styles.icon} src="/overview.png" alt="" />
+        <h1 className={styles.name}>Overview</h1>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
